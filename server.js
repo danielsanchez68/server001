6e06d132c5c260df0e320be556b358eb70904dab
danielsanchez68 <danielsanchez68@hotmail.com>
2022-02-19 10:40:22 -0300

    servidor express

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express'       // "type": "module" en linea 2 package.json
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send('<h1 style="color:blue;">Bienvenidos al servidor express</h1>')
+})
+
+let visitas = 0
+app.get('/visitas', (req,res) => {
+    res.send(`La cantidad de visitas es ${++visitas}`)
+})
+
+app.get('/fyh', (req,res) => {
+    res.send({fyh: new Date().toLocaleString()})
+})
+
+const PORT = process.env.PORT || 8080
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+})
+server.on("error", error => console.log(`Error en servidor ${error}`))
